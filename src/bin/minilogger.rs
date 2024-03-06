262bd7ac8b10a45bc102db4f3cf06dd29c153f84
Liam Davies <mangobox228@hotmail.com.au>
2024-03-06 19:49:01 +1100

    Fixed some additional build options.
    Also requires the nightly build settings.

--- src/bin/minilogger.rs
+++ src/bin/minilogger.rs
@@ -1,17 +1,14 @@
 #![no_std]
 #![no_main]
 
-use core::sync::atomic::AtomicU32;
-
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::{adc, bind_interrupts};
-use embassy_stm32::peripherals::ADC1;
+use embassy_stm32::bind_interrupts;
 use {defmt_rtt as _, panic_probe as _};
 
-/*bind_interrupts!(struct Irqs {
-    ADC1_2 => adc::InterruptHandler<ADC1>;
-});*/
+bind_interrupts!(struct Irqs {
+    //ADC1_2 => adc::InterruptHandler<ADC1>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
