6e36a785452563976b71e65b7e67be5e1f8e3d69
Liam Davies <mangobox228@hotmail.com.au>
2024-05-12 23:13:14 +1000

    Added ADC code working for STM32F0

--- src/bin/minilogger.rs
+++ src/bin/minilogger.rs
@@ -3,73 +3,37 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::bind_interrupts;
+use embassy_stm32::adc::{Adc, SampleTime};
+use embassy_stm32::peripherals::ADC;
+use embassy_stm32::{adc, bind_interrupts};
+use embassy_time::{Delay, Duration, Timer};
+use embedded_hal::blocking::delay::DelayUs;
 use {defmt_rtt as _, panic_probe as _};
-use adxl345_driver2::{i2c::Device, Adxl345Reader, Adxl345Writer};
-use embassy_stm32::i2c;
-use embassy_stm32::i2c::I2c;
-use embassy_stm32::peripherals;
-use embassy_stm32::dma::NoDma;
-use embassy_stm32::time::hz;
-use embassy_time::Timer;
 
-
-
-/// Output scale is 4mg/LSB.
-const SCALE_MULTIPLIER: f64 = 0.004;
-/// Average Earth gravity in m/s²
-const EARTH_GRAVITY_MS2: f64 = 9.80665;
-
-
-bind_interrupts!(struct Irqs { 
-    //ADC1_2 => adc::InterruptHandler<ADC1>;
+bind_interrupts!(struct Irqs {
+    ADC1_COMP => adc::InterruptHandler<ADC>;
 });
-
-#[embassy_executor::task]
-async fn adxl() {
-    let p = embassy_stm32::init(Default::default());
-
-    let mybus = I2c::new(
-        p.I2C1,
-        p.PB6,
-        p.PB7,
-        Irqs,
-        NoDma,
-        NoDma,
-        hz(100000),
-        Default::default(),
-    );
-
-    bind_interrupts!(struct Irqs {
-        I2C1 => i2c::EventInterruptHandler<peripherals::I2C1>, i2c::ErrorInterruptHandler<peripherals::I2C1>;
-    });
-    
-    let mut adxl345 = Device::new(mybus).unwrap();
-
-    adxl345
-        .set_data_format(8)
-        .unwrap();
-    // Set measurement mode on.
-    adxl345
-        .set_power_control(8)
-        .unwrap();
-
-    loop{
-        // Set full scale output and range to 2G.
-
-
-        let (x, y, z) = adxl345
-            .acceleration()
-            .unwrap();
-        let x = x as f64 * SCALE_MULTIPLIER * EARTH_GRAVITY_MS2;
-        let y = y as f64 * SCALE_MULTIPLIER * EARTH_GRAVITY_MS2;
-        let z = z as f64 * SCALE_MULTIPLIER * EARTH_GRAVITY_MS2;
-        info!("X-axis = {}, Y-axis = {}, Z-axis = {}", x, y, z);
-        Timer::after_millis(150).await;
-    }
-}
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    _spawner.spawn(adxl()).unwrap();
+    let p = embassy_stm32::init(Default::default());
+    info!("Hello World!");
+
+    let mut adc = Adc::new(p.ADC, Irqs, &mut Delay {});
+    adc.set_sample_time(SampleTime::Cycles71_5);
+    let mut pin = p.PA1;
+
+    let mut vrefint = adc.enable_vref(&mut Delay {});
+    let vrefint_sample = adc.read(&mut vrefint).await;
+    let convert_to_millivolts = |sample| {
+        const VREFINT_MV: u32 = 1230; // mV
+
+        (u32::from(sample) * VREFINT_MV / u32::from(vrefint_sample)) as u16
+    };
+
+    loop {
+        let v = adc.read(&mut pin).await;
+        info!("--> {} - {} mV", v, convert_to_millivolts(v));
+        Timer::after_millis(100).await;
+    }
 }
